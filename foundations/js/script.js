e110e443f65ff0c09ca05d8b7058d32cf7c323bb
lucenas <samlucena2@gmail.com>
2022-07-02 16:38:41 +1000

    Add rps functions

--- foundations/js/script.js
+++ foundations/js/script.js
@@ -21,3 +21,70 @@ button.addEventListener('click', (e) => {
         console.log("Alt key used");
     }
 });
+
+// ---------- Rock, Paper, Scissors code ----------
+const ROCK = 'rock';
+const PAPER = 'paper';
+const SCISSORS = 'scissors';
+const GUESSES = [ROCK, ROCK, SCISSORS];
+
+const PLAYER_WINS = 0;
+const COMPUTER_WINS = 1;
+const DRAW = 2;
+
+const ARGUMENT_ERROR_MESSAGE = 'ERROR';
+
+// Return either 'rock', 'paper', or 'scissors'.
+function computerGuess() {
+    return GUESSES[Math.floor(Math.random() * 3)];
+}
+
+// Return 0 if the player wins, 1 if comuter wins, 2 if they draw.
+// computerGuess and playerGuess should be strings of 'rock', 'paper',
+// or 'scissors', otherwise 'ERROR' is returned.
+function playRPS(computerGuess, playerGuess) {
+    if (typeof(computerGuess) != 'string' || typeof(playerGuess) != 'string'
+            || arguments.length != 2) {
+        return ARGUMENT_ERROR_MESSAGE;
+    }
+
+    switch (computerGuess) {
+        case ROCK:
+            if (playerGuess == PAPER) {
+                return PLAYER_WINS;
+            } else if (playerGuess == SCISSORS) {
+                return COMPUTER_WINS;
+            } else if (playerGuess == ROCK) {
+                return DRAW;
+            }
+            break;
+
+        case PAPER:
+            if (playerGuess == SCISSORS) {
+                return PLAYER_WINS;
+            } else if (playerGuess == ROCK) {
+                return COMPUTER_WINS;
+            } else if (playerGuess == PAPER) {
+                return DRAW;
+            }
+            break;
+
+        case SCISSORS:
+            if (playerGuess == ROCK) {
+                return PLAYER_WINS;
+            } else if (playerGuess == PAPER) {
+                return COMPUTER_WINS;
+            } else if (playerGuess == SCISSORS) {
+                return DRAW;
+            }
+            break;
+    }
+
+    // If we reach here, an input is wrong.
+    return ARGUMENT_ERROR_MESSAGE;
+}
+
+let rockBtn = document.querySelector('#rock');
+let paperBtn = document.querySelector('#paper');
+let scissorsButton = document.querySelector('#scissors');
+
