f6cd559e57216ad66fd0e0ad1d1e69ce52c09c15
Sam Lucena <samlucena2@gmail.com>
2022-07-02 13:21:11 +1000

    Write test js functions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+let sum = (a, b) => a + b
+
+function capitalise(str) {
+    return str[0].toUpperCase() + str.slice(1)
+}
+
+function lastLetter(str) {
+    return str[str.length - 1]
+}
+
 console.log("Hello, World!")
+console.log("" + sum(2, 3) + " is the sum of 2 and 3.")
+console.log(capitalise("nothing"))
+console.log(lastLetter("This should only return 1"))
