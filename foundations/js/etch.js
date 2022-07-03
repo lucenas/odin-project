2d1c04489e3541e1d334845afda3c9bdb4df8d97
lucenas <samlucena2@gmail.com>
2022-07-03 17:20:22 +1000

    Add random button
    Etch-a-sketch receives a button which makes every hover produce a
    random colour on that box.

--- foundations/js/etch.js
+++ foundations/js/etch.js
@@ -1,12 +1,13 @@
 DEFAULT_COLOR = '#857E7B';
+HIGHLIGHT_ONE = '#FFA500';
 
 // Add a grid of size numRows * numCols inside the html element
 // container. This grid will have a background colour of 
 // color() and each will be given class etch-box and give any 
 // hover events to boxHover().
 // Container is first cleared of all children.
-function makeGrid(container, numRows = 10, numCols = 10, 
-        color = () => {return DEFAULT_COLOR}) {
+function makeGrid(container, numRows = 10, numCols = 10,
+    color = () => { return HIGHLIGHT_ONE; }) {
     // Clear the current children.
     while (container.lastChild) {
         container.removeChild(container.lastChild);
@@ -18,30 +19,55 @@ function makeGrid(container, numRows = 10, numCols = 10,
 
             let box = document.createElement('div');
             box.classList.add('etch-box');
-            box.style.backgroundColor = color();
+            box.style.backgroundColor = DEFAULT_COLOR;
             box.style.width = `${100 / numRows}%`;
             box.style.height = `${100 / numCols}%`;
             box.addEventListener('mouseover', (e) => {
                 let source = e.target || e.srcElement;
-                source.style.backgroundColor = 'green';
+                source.style.backgroundColor = color();
             });
             container.appendChild(box);
         }
     }
 }
 
-// Set the style.backgroundColor of each child of container to color.
+// Set the style.backgroundColor of each child of container to color().
 function resetColors(container, color) {
     for (i = 0; i < container.children.length; i++) {
-        container.children[i].style.backgroundColor = color;
+        container.children[i].style.backgroundColor = color();
     }
+}
+
+// Return random whole number in range [start, end].
+function randInt(start, end) {
+    return Math.floor(Math.random() * 255);
+}
+
+function randRGB() {
+    return `rgb(${randInt(1, 255)}, ${randInt(1, 255)}, ${randInt(1, 255)})`;
 }
 
 // Make default 10 x 10 grid.
 let etchContainer = document.querySelector('#etch-container');
 makeGrid(etchContainer);
 
+let numRows = 10;
+let numCols = 10;
+
 // Settings buttons
 let resetButton = document.querySelector('#etch-reset');
-resetButton.addEventListener('click', () => resetColors(etchContainer, 
-        DEFAULT_COLOR));
+resetButton.addEventListener('click', () => resetColors(etchContainer,
+    () => { return DEFAULT_COLOR; }));
+
+let randomButton = document.querySelector('#etch-random');
+let randomEnabled = false;
+randomButton.addEventListener('click', () => {
+    randomEnabled = !randomEnabled;
+    if (!randomEnabled) {
+        randomButton.classList.remove('enabled');
+        makeGrid(etchContainer, numRows, numCols);
+    } else {
+        randomButton.classList.add('enabled');
+        makeGrid(etchContainer, numRows, numCols, randRGB);
+    }
+});
